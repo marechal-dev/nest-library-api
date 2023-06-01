869288215dc84617cfee358b24ce5b7ca661426d
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-06-01 17:21:32 -0300

    feat: add infra and persistence modules

--- src/application/application.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CreateBookUseCase } from "./use-cases/create-book";
-
-@Module({
-	providers: [CreateBookUseCase],
-})
-export class ApplicationModule {}
